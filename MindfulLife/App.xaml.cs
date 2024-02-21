fc4240b798375c5a45d0e290c2ac158dd52c1d1a
micihime <hroncovam@yahoo.com>
2024-02-21 11:24:44 +0100

    Add project files.

---
+++ MindfulLife/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MindfulLife
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MindfulLife/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MindfulLife
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
